f70a8f4d2d9a4a815b86bc04d3672d90c92434d3
xmcxtgb <43981763+xmcxtgb@users.noreply.github.com>
2024-10-26 21:27:35 +0800

    Update patch.ts

--- patch.ts
+++ patch.ts
@@ -2,12 +2,8 @@ import { PatchApps } from './src/types';
 
 export const PatchAppList: PatchApps = [
 {
-  packgeName: '应用包名1',
-  appName: '应用名称1',
-},
-{
-  packgeName: '应用包名2',
-  appName: '应用名称2',
+  packgeName: 'com.cn21.ecloud',
+  appName: '天翼云盘',
 },
 ...  
 ];
