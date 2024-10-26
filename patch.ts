cfdb758f74d88df687f6cb1b79ebe94643800368
xmcxtgb <43981763+xmcxtgb@users.noreply.github.com>
2024-10-26 21:12:04 +0800

    Update patch.ts

--- patch.ts
+++ patch.ts
@@ -1,5 +1,13 @@
 import { PatchApps } from './src/types';
 
 export const PatchAppList: PatchApps = [
-  
+{
+  packgeName: '应用包名1',
+  appName: '应用名称1',
+},
+{
+  packgeName: '应用包名2',
+  appName: '应用名称2',
+},
+...  
 ];
